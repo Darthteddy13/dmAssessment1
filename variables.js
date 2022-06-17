f28a05ff46db9bd39dad87d4b9a92ec1572cb1d5
Kevin Taylor <kevintay96@gmail.com>
2022-06-17 10:18:09 -0600

    finished variables.js

--- variables.js
+++ variables.js
@@ -3,16 +3,17 @@
 
 //CODE HERE 
 
+let myName = "Kevin Taylor";
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
-
+let faveNum = 13;
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
-
+let lovesCode = true;
 //////////////////PROBLEM 4////////////////////
 /*
   Write an if-statement that checks if the faveNum variable is 
@@ -21,10 +22,24 @@
 */
 
 //CODE HERE
-
+//lol I put chose 13 before I knew this test was here
+if (faveNum === 13)
+  {
+    console.log(`lucky 13!`);
+  }
+else
+  {
+    console.log(`not lucky 13`);
+  }
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
 
 //CODE HERE
+
+for (let i = 0; i < 5; i++)
+  {
+    console.log(faveNum);
+    // console.log(i+1);
+  }
