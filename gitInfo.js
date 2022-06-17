ca191dee2c84f1bfa295e5d8650e7b7fe51f97f9
Kevin Taylor <kevintay96@gmail.com>
2022-06-17 10:35:11 -0600

    finished gitInfo.js

--- gitInfo.js
+++ gitInfo.js
@@ -10,6 +10,9 @@
 */
 
 //CODE HERE
+ 
+let gitDefinition = "Git is a version control system which allows you to creat different commits (versions) in order to recall previous versions for future reference or use";
+
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,6 +22,9 @@
 
 //CODE HERE
 
+let gitHubDefinition = "GitHub is an online version control system. allowing you to upload commits and recall previous versions but also it allows others to access and upload their commits to the same repo, allowing groups to work on one project no matter the location";
+
+
 //////////////////PROBLEM 3////////////////////
 /*
     Create a variable called 'gitInitDefinition'.  
@@ -26,6 +32,8 @@
 */
 
 //CODE HERE
+
+let gitInitDefinition = "git init allows you to initialize the current director to be used with git as its' version control system";
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -35,6 +43,8 @@
 
 //CODE HERE
 
+let gitCloneDefinition = "git clone allows you to copy an existing git repo to another directory";
+
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,6 +52,8 @@
 */
 
 //CODE HERE
+
+let gitStatusDefinition = "Git status shows you the status of the directory and tells if there are any commits waiting to be done";
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -54,6 +66,9 @@
 
 //CODE HERE
 
+let gitAddDefinition = "git add checks for and readies changes to be commited";
+let gitAddCode = "git add . ";
+
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -65,6 +80,8 @@
 
 //CODE HERE
 
+let gitCommitDefinition = "git commit finalizes made changes and saves it as a new version of the file or directory you were working on"
+let gitCommitCode = "git commit -m `message of what you changed this commit` ";
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +90,5 @@
 */
 
 //CODE HERE
+
+let gitPushDefinition = "git push sends the latest commits to git hub or whatever online repo system you have setup ";
